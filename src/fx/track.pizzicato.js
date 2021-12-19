406214e7c4a34e9f815ec69653ba8dca19c10fce
ignatif <ignatif@gmail.com>
2021-12-19 12:45:32 +0200

    pizzicato

--- src/fx/track.pizzicato.js
+++ src/fx/track.pizzicato.js
@@ -2,9 +2,11 @@ import React, { useState, useContext, useEffect } from 'react'
 import { AppContext } from 'context'
 import useSound from 'use-sound'
 
+import Pizzicato from 'pizzicato'
+
 const Track = ({ track }) => {
   const { selectedTrack, setSelectedTrack, screen } = useContext(AppContext)
-  const [playTrack, { stop, sound }] = useSound(track.url)
+  const [playTrack, { stop }] = useSound(track.url)
   const [isActive, setActive] = useState(
     selectedTrack && selectedTrack.name === track.name,
   )
@@ -12,7 +14,23 @@ const Track = ({ track }) => {
   useEffect(() => {
     if (selectedTrack && selectedTrack.name === track.name) {
       setActive(true)
-      playTrack()
+      // var sound = new Pizzicato.Sound({ source: 'input' }, function () {
+      var sound = new Pizzicato.Sound(track.url, function () {
+        var distortion = new Pizzicato.Effects.Distortion({
+          gain: 0.8,
+        })
+
+        var pingPongDelay = new Pizzicato.Effects.PingPongDelay({
+          feedback: 0.6,
+          time: 0.4,
+          mix: 0.5,
+        })
+        sound.addEffect(pingPongDelay)
+        sound.addEffect(distortion)
+        sound.play()
+      })
+
+      // playTrack()
     } else {
       setActive(false)
       stop()
